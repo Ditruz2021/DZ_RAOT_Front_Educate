c6aa386364c3b5b747fed8b1c796201fcd8c82c5
tharinthorn01 <tharinthorn33@gmail.com>
2026-03-08 20:05:23 +0700

    fix path

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router'
 
 // Vuetify
-import 'vuetify/styles'
+import 'vuetify/lib/styles/main.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
